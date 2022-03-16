2550c9c20fb04cf04d7065dc87ee5d85d9a69be6
sharon <sharonvazana@gmail.com>
2022-03-16 19:41:48 +0200

    nav bar and overall site design

--- src/main.js
+++ src/main.js
@@ -10,3 +10,33 @@ app.get('/',(req, res)=>{
 app.listen(port, ()=>{
     console.log('server is up and runing- cheacking');
 })
+
+
+// nav bar and page display 
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+// end of nav bar and page display
