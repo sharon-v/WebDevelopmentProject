b9c29fafb7a694253bbb9d2f0c680261d50c556b
yarden hovav <hovav4321@gmail.com>
2022-03-18 18:44:45 +0200

    fixing settings in sign up files

--- src/signup.js
+++ src/signup.js
@@ -3,12 +3,13 @@ function setFormMessage(formElement, type, message) {
 
     messageElement.textContent = message;
     messageElement.classList.remove("signUpForm__message--success", "signUpForm__message--error");
-    messageElement.classList.add(`form__message--${type}`);
+    messageElement.classList.add(`signUpForm__message--${type}`);
 }
 
 function setInputError(inputElement, message) {
     inputElement.classList.add("signUpForm__input--error");
     inputElement.parentElement.querySelector(".signUpForm__input-error-message").textContent = message;
+
 }
 
 function clearInputError(inputElement) {
@@ -21,24 +22,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.querySelector("#linkLogin").addEventListener("click", e => {
         e.preventDefault();
-        // loginForm.classList.remove("form--hidden");
-        // createAccountForm.classList.add("form--hidden");
+        //need to add implementation for transmition to login page
+   
     });
-
+    
+    //when press on sign up button 
     signUpForm.addEventListener("submit", e => {
         e.preventDefault();
 
         // Perform your AJAX/Fetch login
 
-        setFormMessage(loginForm, "error", "Invalid username/password combination");
     });
 
     document.querySelectorAll(".signUpForm__input").forEach(inputElement => {
         inputElement.addEventListener("blur", e => {
+            // chack if this the condition we want - if at all
             if (e.target.id === "signUp__firstName" && e.target.value.length > 0 && e.target.value.length < 10) {
                 setInputError(inputElement, "First name must be at least 10 characters in length");
             }
         });
+        
 
         inputElement.addEventListener("input", e => {
             clearInputError(inputElement);
