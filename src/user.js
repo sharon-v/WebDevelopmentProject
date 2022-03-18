52fee08434b10f70a80c702700a72f2da402d65f
chenBenTolila <chenbt266@gmail.com>
2022-03-18 13:59:00 +0200

    fixing login.css and adding user class

---
+++ src/user.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(email,firstName,lastName,id,birth_date){
+      this.firstName=firstName;
+      this.lastName=lastName
+      this.id=id;
+      this.birth_date=birth_date;
+      this.email=email;
+    }
+}
