ebf7e0a5a8703e32f0172c137c2affd9d2888869
chenBenTolila <chenbt266@gmail.com>
2022-03-20 00:08:37 +0200

    adding functionality to modelFirebse.js

--- src/model_firebase.js
+++ src/model_firebase.js
@@ -13,8 +13,22 @@ const firebaseApp = initializeApp({
     appId: "1:226441492460:web:522fc6bb71346b94ba72c9",
     measurementId: "G-3JRYLKGGNL"
 });
+
+// init services
 const analytics = getAnalytics(app);
 const auth = getAuth(firebaseApp);
 const db = getFirestore(firebaseApp);
 
+// collection reference
+const usersColRef = collection(db, 'users');
 
+// get collection data
+getDocs(usersColRef).then((snapshot) => {
+    let users = [];
+    snapshot.docs.forEach((doc) => {
+        users.push({ ...doc.data(), id: doc.id })
+    })
+    console.log(users);
+}).catch( err => {
+    console.log(err.message);
+})
