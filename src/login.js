a241aae27277194debfbefddcdadb4055341d79c
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2022-03-20 00:01:04 +0200

    welcome page

--- src/login.js
+++ src/login.js
@@ -6,6 +6,27 @@ function setFormMessage(formElement, type, message) {
     messageElement.classList.add(`form__message--${type}`);
 }
 
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "Formget" && password == "formget#123"){
+    alert ("Login successfully");
+    window.location = "success.html"; // Redirecting to other page.
+    return false;
+    }
+    else{
+    attempt --;// Decrementing by one.
+    alert("You have left "+attempt+" attempt;");
+    // Disabling fields after 3 attempts.
+    if( attempt == 0){
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = true;
+    document.getElementById("submit").disabled = true;
+    return false;
+    }
+    }
+    }
+
 function setInputError(inputElement, message) {
     inputElement.classList.add("form__input--error");
     inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
