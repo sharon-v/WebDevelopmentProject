aaee013ad32b495ffa2b79384bdb8b8e2837b0d7
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2022-03-20 17:40:11 +0200

    continue login

--- src/login.js
+++ src/login.js
@@ -1,73 +1,81 @@
 function setFormMessage(formElement, type, message) {
-  const messageElement = formElement.querySelector('.form__message');
+    const messageElement = formElement.querySelector(".form__message");
 
-  messageElement.textContent = message;
-  messageElement.classList.remove('form__message--success', 'form__message--error');
-  messageElement.classList.add(`form__message--${type}`);
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
 }
 
-function validate(){
-    var username = document.getElementById("login__email").value;
-    var password = document.getElementById("login__password").value;
-    if ( username == "Formget" && password == "formget#123"){
-    alert ("Login successfully");
-    window.location = "welcom.html"; // Redirecting to other page.
-    return false;
-    }
-    else
-    alert ("failed to login, please try again");
-    }
+// function validate(){
+//     var email = document.getElementById("login__email").value;
+//     var password = document.getElementById("login__password").value;
+//     if ( email == "Formget" && password == "formget#123"){
+//     alert ("Login successfully");
+//     window.location = "welcom.html"; // Redirecting to other page.
+//     return false;
+//     }
+//     else
+//     alert ("failed to login, please try again");
+//     }
+
 const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const username = loginForm['login__email'].value;
+    // e.preventDefault();
+    const email = loginForm['login__email'].value;
     const password = loginForm['login__password'].value;
-    
+    alert("chen");
+    signInWithEmailAndPassword(auth,email, password).then((cred)=>{
+        window.location = "welcom.html";
+        alert ("Login successfully");
+    })
+    .catch((err)=>{
+        console.log("failed to login, please try again");
+    })
 })
 
 function setInputError(inputElement, message) {
-  inputElement.classList.add('form__input--error');
-  inputElement.parentElement.querySelector('.form__input-error-message').textContent = message;
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
 }
 
 function clearInputError(inputElement) {
-  inputElement.classList.remove('form__input--error');
-  inputElement.parentElement.querySelector('.form__input-error-message').textContent = '';
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const loginForm = document.querySelector('#login');
-  const createAccountForm = document.querySelector('#createAccount');
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
 
-  // document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-  //     e.preventDefault();
-  //     loginForm.classList.add("form--hidden");
-  //     createAccountForm.classList.remove("form--hidden");
-  // });
+    // document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+    //     e.preventDefault();
+    //     loginForm.classList.add("form--hidden");
+    //     createAccountForm.classList.remove("form--hidden");
+    // });
 
-  // document.querySelector("#linkLogin").addEventListener("click", e => {
-  //     e.preventDefault();
-  //     loginForm.classList.remove("form--hidden");
-  //     createAccountForm.classList.add("form--hidden");
-  // });
+    // document.querySelector("#linkLogin").addEventListener("click", e => {
+    //     e.preventDefault();
+    //     loginForm.classList.remove("form--hidden");
+    //     createAccountForm.classList.add("form--hidden");
+    // });
 
-  loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
 
-    // Perform your AJAX/Fetch login
+        // Perform your AJAX/Fetch login
 
-    setFormMessage(loginForm, 'error', 'Invalid username/password combination');
-  });
-
-  document.querySelectorAll('.form__input').forEach((inputElement) => {
-    inputElement.addEventListener('blur', (e) => {
-      if (e.target.id === 'signupUsername' && e.target.value.length > 0 && e.target.value.length < 10) {
-        setInputError(inputElement, 'Username must be at least 10 characters in length');
-      }
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
     });
 
-    inputElement.addEventListener('input', (e) => {
-      clearInputError(inputElement);
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
     });
-  });
 });
