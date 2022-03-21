c0819e86cb392fc404a8fcf3d45da5c964333eb0
chenBenTolila <chenbt266@gmail.com>
2022-03-21 21:55:08 +0200

    fixing linter problems

--- src/user.js
+++ src/user.js
@@ -7,18 +7,3 @@ class User {
     this.phoneNumber=phoneNumber;
   }
 }
-
-/* we need to change it to a different implematation */
-class UserList {
-  constructor(users = []) {
-    this.list = users;
-  }
-
-  addUser(user) {
-    this.list.push(user);
-  }
-
-  getUserById(id) {
-    return this.list.find((user) => user.id == id);
-  }
-}
