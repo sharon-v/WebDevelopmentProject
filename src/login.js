3290f00f105bcf13fdae616a394808ff3dd5ee92
chenBenTolila <chenbt266@gmail.com>
2022-03-21 23:46:34 +0200

    error message in login page

--- src/login.js
+++ src/login.js
@@ -1,18 +1,25 @@
-const loginForm = document.querySelector('#login');
-loginForm.addEventListener('submit', (e) => {
-  // e.preventDefault();
-  const email = loginForm['login__email'].value;
-  const password = loginForm['login__password'].value;
-  loginUser(email, password);
-});
+function setInputError(inputElement, message) {
+  inputElement.classList.add("loginForm__input--error");
+  inputElement.parentElement.querySelector(".loginForm__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+  inputElement.classList.remove("loginForm__input--error");
+  inputElement.parentElement.querySelector(
+".loginForm__input-error-message").textContent = "";
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.querySelector('#login');
-
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
-    // Perform your AJAX/Fetch login
-    window.location.href = 'welcom.html';
+    // check if this the condition we want - if at all
+    if (loginForm['login__password'].value.length < 6) {
+      setInputError(inputElement, 'password must be at least 6 characters in length');
+    }
+    else{
+      // Perform your AJAX/Fetch login
+      window.location.href = 'welcom.html';
+    }
   });
 });
