4a568f75fd4280e4df7624e37260c87180d9c081
yarden hovav <hovav4321@gmail.com>
2022-04-24 12:26:57 +0300

    adding scrips of navbar and footer and adding them to the login page, registration page and welcome page

--- src/new pages/footer.js
+++ src/new pages/footer.js
@@ -1,7 +1,7 @@
-fetch('navbartemp.html')
+fetch('footer.html')
     .then((res) => res.text())
     .then((text) => {
-      const oldelem = document.querySelector('script#replace_with_navbar');
+      const oldelem = document.querySelector('script#replace_with_footer');
       const newelem = document.createElement('div');
       newelem.innerHTML = text;
       oldelem.parentNode.replaceChild(newelem, oldelem);
