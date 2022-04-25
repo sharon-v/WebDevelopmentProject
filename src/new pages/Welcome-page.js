30694d3f2c5ca3fddd73f7b7208ed4cfe9391a0a
yarden hovav <hovav4321@gmail.com>
2022-04-25 22:07:11 +0300

    trying to fix the carousel - still not working

---
+++ src/new pages/Welcome-page.js
@@ -0,0 +1,12 @@
+// Activate Carousel
+$("#carousel-1").carousel();
+
+// Enable Carousel Indicators
+$(".carousel-item").click(function(){
+  $("#carousel-1").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#carousel-1").carousel("prev");
+});
