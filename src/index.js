39d81ce321f94927ce7a63a78e02b1660bc63002
sharon <sharonvazana@gmail.com>
2022-05-01 02:40:47 +0300

    footer and nav 2nd fix

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   // res.send('<h1>The web is on the air<h1>');
-  res.sendFile('/components/Welcome-page.html', { root: __dirname });
+  res.sendFile('/components/welcome-page.html', { root: __dirname });
 });
 
 app.listen(port, () => {
