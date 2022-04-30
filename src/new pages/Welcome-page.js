14bd84d5b71f20d9583fdf0ea461012d1a594342
sharon <sharonvazana@gmail.com>
2022-04-30 21:52:29 +0300

    fixing some lint errors

--- src/new pages/Welcome-page.js
+++ src/new pages/Welcome-page.js
@@ -1,12 +1,12 @@
 // Activate Carousel
-$("#carousel-1").carousel();
+$('#carousel-1').carousel();
 
 // Enable Carousel Indicators
-$(".carousel-item").click(function(){
-  $("#carousel-1").carousel(1);
+$('.carousel-item').click(function () {
+  $('#carousel-1').carousel(1);
 });
 
 // Enable Carousel Controls
-$(".left").click(function(){
-  $("#carousel-1").carousel("prev");
+$('.left').click(function () {
+  $('#carousel-1').carousel('prev');
 });
