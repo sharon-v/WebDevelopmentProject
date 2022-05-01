3c70b5052d98fcc50c83623a01023f365308f3c1
chenBenTolila <chenbt266@gmail.com>
2022-05-01 17:06:09 +0300

    fixing linter problems

--- src/components/registration.js
+++ src/components/registration.js
@@ -1,34 +1,34 @@
-function sendJSON() {
-	// let result = document.querySelector('.result');
-	let name = document.querySelector('#name');
-	let email = document.querySelector('#email');
+// function sendJSON() {
+// let result = document.querySelector('.result');
+// let name = document.querySelector('#name');
+// let email = document.querySelector('#email');
 
-	// Creating a XHR object
-	let xhr = new XMLHttpRequest();
-	let url = "submit.php";
+// Creating a XHR object
+// let xhr = new XMLHttpRequest();
+// let url = "submit.php";
 
-	// open a connection
-	xhr.open("POST", url, true);
+// open a connection
+// xhr.open("POST", url, true);
 
-	// Set the request header i.e. which type of content you are sending
-	xhr.setRequestHeader("Content-Type", "application/json");
+// Set the request header i.e. which type of content you are sending
+// xhr.setRequestHeader("Content-Type", "application/json");
 
-	// Create a state change callback
-	xhr.onreadystatechange = function () {
-		if (xhr.readyState === 4 && xhr.status === 200) {
+// Create a state change callback
+// xhr.onreadystatechange = function () {
+// if (xhr.readyState === 4 && xhr.status === 200) {
 
-			// Print received data from server
-			result.innerHTML = this.responseText;
+// Print received data from server
+// result.innerHTML = this.responseText;
 
-		}
-	};
+// }
+// };
 
-	// Converting JSON data to string
-	var data = JSON.stringify({ "name": name.value, "email": email.value });
+// Converting JSON data to string
+// var data = JSON.stringify({ "name": name.value, "email": email.value });
 
-	// Sending data with the request
-	xhr.send(data);
-}
+// Sending data with the request
+// xhr.send(data);
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
 	const registerForm = document.querySelector('#regestraion');
