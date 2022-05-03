2d5d1f7c34514e8c052207f93efbbac6d5c694c2
sharon <sharonvazana@gmail.com>
2022-05-03 20:39:58 +0300

    navbar id

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,5 @@
+import { fbAuth } from '../firebase/data.js';
+
 fetch('../components/navbar.html')
   .then((res) => res.text())
   .then((text) => {
@@ -6,3 +8,25 @@ fetch('../components/navbar.html')
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem, oldelem);
   });
+
+// gets current user
+fbAuth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log('inside user if');
+    // check if manager
+
+    // check if customer
+
+    var uid = user.uid;
+    // ...
+  } else {
+    console.log('inside user else');
+    // User is signed out
+    document.getElementById('nav_home_btn').style.visibility = 'hidden';
+    document.getElementById('nav_catalog_btn').style.visibility = 'hidden';
+    document.getElementById('nav_catalog_btn').style.visibility = 'hidden';
+    document.getElementById('nav_wishlist_btn').style.visibility = 'hidden';
+    document.getElementById('nav_profile_btn').style.visibility = 'hidden';
+    document.getElementById('nav_logout_btn').style.visibility = 'hidden';
+  }
+});
