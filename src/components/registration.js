4bcd5b355e0197ef5d397d79fa5185db83c1f28a
chenBenTolila <chenbt266@gmail.com>
2022-05-03 18:25:06 +0300

    dividing the sign up to manager and customer

--- src/components/registration.js
+++ src/components/registration.js
@@ -1,4 +1,4 @@
-import { state, fbAuth, dbCustomers } from '../firebase/data.js'
+import { fbAuth, dbCustomers, dbManager } from '../firebase/data.js'
 
 console.log('enter');
 
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	var btn = document.getElementById('submit');
 	// when press on sign up button
 	btn.addEventListener('click', (e) => {
-		e.preventDefault();
+		e.preventDefault();  // IMPORTANT! so the db functions could work, DO NOT REMOVE
 		console.log('clicked on register');
 		const fname = document.getElementById('registerfName').value;
 		const lname = document.getElementById('registerlName').value;
@@ -19,15 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
 		const password = document.getElementById('registerPassword').value;
 		const passwordConfirmation = document.getElementById('registerConfirmPassword').value;
 		const managerPasscode = document.getElementById('registerManagerPasscode').value;
+		const isCbChecked = document.getElementById('registerManagerCheckBox').isCbChecked;
 
-		// if (checkEmail(email) == false) {
-		// 	// write password confirmation message
-		// }
-
-		// if (checkPasswordConfiramtion(password, passwordConfirmation) == false) {
-		// 	// write password confirmation message
-		// }
-
+		// writing data collected for testing
 		console.log(fname);
 		console.log(lname);
 		console.log(birthdate);
@@ -36,6 +30,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		console.log(password);
 		console.log(passwordConfirmation);
 		console.log(managerPasscode);
+
+		if (isCbChecked) {
+			managerSignUp(fname, lname, birthdate, phoneNumber, email, password, passwordConfirmation, managerPasscode);
+		}
+		else {
+			customerSignUp(fname, lname, birthdate, phoneNumber, email, password, passwordConfirmation);
+		}
 
 		// forgot password - reseting with firebasse 
 		// fbAuth.sendPasswordResetEmail(email)
@@ -49,7 +50,25 @@ document.addEventListener('DOMContentLoaded', () => {
 		// 		// ..
 		// 	});
 
-		// creating new user with firebase Auth
+	});
+});
+
+
+/* check for invalid password or password confirmation */
+function checkPasswordConfirmation(password, passwordConfirmation) {
+	// check if the passwords are equal 
+	if (password != passwordConfirmation) {
+		alert('passwords do not match');
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+function customerSignUp(fname, lname, birthdate, phoneNumber, email, password, passwordConfirmation) {
+	if (checkPasswordConfirmation(password, passwordConfirmation)) {
+
 		fbAuth
 			.createUserWithEmailAndPassword(email, password)
 			.then((userCredential) => {
@@ -61,11 +80,11 @@ document.addEventListener('DOMContentLoaded', () => {
 					lname: lname,
 					birthdate: birthdate,
 					phoneNumber: phoneNumber,
-					email: email
+					email: email   // need to consider removing it
 				})
 					.then(() => {
 						console.log('Document successfully written!');
-						location.replace('home-page.html');   // move the user to the home page
+						location.replace('home-page.html');   // redirect the user to the home page
 					})
 					.catch((error) => {
 						// success in saving the user to Auth but failed to save him in the collection
@@ -78,36 +97,50 @@ document.addEventListener('DOMContentLoaded', () => {
 				var errorMessage = error.message;
 				alert(errorMessage);
 			});
+	}
+}
 
-
-		// Perform your AJAX/Fetch login
-		// window.location.href = 'welcom.html';   // ?????????????????????????????
+function managerSignUp(fname, lname, birthdate, phoneNumber, email, password, passwordConfirmation, managerPasscode) {
+	// check if a manager already signed up to our website
+	dbManager.get().then(function (querySnapshot) {
+		if (querySnapshot.empty) {
+			alert('A manager is already signed up to the website');
+			return;
+		}
 	});
-});
 
-
-/* check for invalid password or password confirmation */
-function checkPasswordConfirmation(password, passwordConfirmation) {
-	console.log(password);
-	console.log(passwordConfirmation);
-	// check if the passwords are equal 
-	if (password != passwordConfirmation) {
-		return false;
+	if (!checkPasswordConfirmation(password, passwordConfirmation)) {
+		return;
 	}
-	else {
-		return true;
-	}
-}
 
-// /* check if the email is invalid */
-// function checkEmail(email) {
-// 	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-// }
+	// check if the developer passcode match
 
-function signUpClient() {
-
-}
-
-function signUpManager() {
-
+	fbAuth
+		.createUserWithEmailAndPassword(email, password)
+		.then((userCredential) => {
+			// Signed in
+			var user = userCredential.user;
+			console.log('success, ', user.uid);
+			dbManager.doc(email).set({
+				fname: fname,
+				lname: lname,
+				birthdate: birthdate,
+				phoneNumber: phoneNumber,
+				email: email   // need to consider removing it
+			})
+				.then(() => {
+					console.log('Document successfully written!');
+					location.replace('home-page.html');   // redirect the user to the home page
+				})
+				.catch((error) => {
+					// success in saving the user to Auth but failed to save him in the collection
+					// so we need to delete user from authentication
+					console.error('Error writing document: ', error);
+				});
+		})
+		.catch((error) => {
+			console.log('fail');
+			var errorMessage = error.message;
+			alert(errorMessage);
+		});
 }
--- src/firebase/data.js
+++ src/firebase/data.js
@@ -1,28 +1,26 @@
-// import { state } from "../../firebase.js";
-
 // Your web app's Firebase configuration
 export const state = {
     firebaseConfig: {
-        apiKey: "AIzaSyAlk5hRiLNudNrTepVbfSd5ClZVZEip2LA",
-        authDomain: "holysheetsweb.firebaseapp.com",
-        projectId: "holysheetsweb",
-        storageBucket: "holysheetsweb.appspot.com",
-        messagingSenderId: "440529465617",
-        appId: "1:440529465617:web:9be0d40688f07151849cb0",
-        measurementId: "G-MS9HPT4M16",
+        apiKey: 'AIzaSyAlk5hRiLNudNrTepVbfSd5ClZVZEip2LA',
+        authDomain: 'holysheetsweb.firebaseapp.com',
+        projectId: 'holysheetsweb',
+        storageBucket: 'holysheetsweb.appspot.com',
+        messagingSenderId: '440529465617',
+        appId: '1:440529465617:web:9be0d40688f07151849cb0',
+        measurementId: 'G-MS9HPT4M16',
     },
 };
 
 
 firebase.initializeApp(state.firebaseConfig);
+
 // Set database variable
-// import "firebase/firestore";
 var database = firebase.firestore();
 var prod = database.collection('products');
-var customers = database.collection("customers");
-// var stores = database.collection("stores");
+var customers = database.collection('customers');
+var manager = database.collection('manager');
 var fbAuth = firebase.auth();
 export { fbAuth as fbAuth };
 export { prod as dbProducts };
 export { customers as dbCustomers };
-// export { stores as dbStores };
+export { manager as dbManager };
