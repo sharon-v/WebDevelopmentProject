e70ba87fcc4b28c84c3839cf514df5ee06477498
chenBenTolila <chenbt266@gmail.com>
2022-05-04 17:49:15 +0300

    continue to fix navbar.js not complete

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,8 @@
 import {
-    fbAuth
+    fbAuth,
+    dbCustomers,
+    dbManager
 } from '../firebase/data.js';
-
-const isManager = -1; // if 1 then true, if 0 then false
 
 fetch('../components/navbar.html')
     .then((res) => res.text())
@@ -13,60 +13,131 @@ fetch('../components/navbar.html')
         oldelem.parentNode.replaceChild(newelem, oldelem);
     });
 
-// gets current user
-fbAuth.onAuthStateChanged((user) => {
-    if (user) {
-        isManager = 1;
-        console.log('inside user if');
-        // check if manager 
-        // TODO: check if working!!!
-        if (dbManager.getElementById(user.email)) {
-            document.getElementById('nav_home_btn').style.visibility = 'hidden';
-            document.getElementById('nav_catalog_btn').style.visibility = 'hidden';
-            document.getElementById('nav_cart_and_badge').style.visibility = 'hidden';
-            document.getElementById('nav_wishlist_btn').style.visibility = 'hidden';
-            document.getElementById('nav_profile_btn').style.visibility = 'hidden';
-            document.getElementById('nav_login_link').style.visibility = 'hidden';
-            document.getElementById('nav_register_link').style.visibility = 'hidden';
+// reconstructing navbar.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('in');
 
+    var isManager = -1; // if 1 it's a manager, if 0 then it's a customer, -1 if not connectedat all
+    isManager = handleNavBar();
+
+    // logout icon function
+    // const logoutBtn = document.querySelector('#nav_logout_btn');
+    const logoutBtn = document.getElementById('nav_logout_btn');
+    logoutBtn.addEventListener('click', function () {
+        if (isManager != -1) {
+            signOutUser();
         }
+    });
 
-
-        // check if customer
-        if (dbManager.getElementById(user.email)) {
-            isManager = 0;
-            document.getElementById('nav_login_link').style.visibility = 'hidden';
-            document.getElementById('nav_register_link').style.visibility = 'hidden';
+    // bed icon function
+    // const nav_bed_btn = document.querySelector('#nav_bed_btn');
+    const nav_bed_btn = document.getElementById('nav_bed_btn');
+    nav_bed_btn.addEventListener('click', () => {
+        if (isManager == -1) {
+            location.replace('welcome-page.html'); // redirect a disconnected user to the home page
+        } else if (isManager == 0) {
+            location.replace('home-page.html'); // redirect a connected user to the home page
         }
+    });
 
-    } else {
-        console.log('inside user else');
-        // User is signed out
-        document.getElementById('nav_home_btn').style.visibility = 'hidden';
-        document.getElementById('nav_catalog_btn').style.visibility = 'hidden';
-        document.getElementById('nav_catalog_btn').style.visibility = 'hidden';
-        document.getElementById('nav_wishlist_btn').style.visibility = 'hidden';
-        document.getElementById('nav_profile_btn').style.visibility = 'hidden';
-        document.getElementById('nav_logout_btn').style.visibility = 'hidden';
-    }
+    // function to send user to right profile page -> profile icon
+    nav_profile_btn.addEventListener('click', () => {
+        if (isManager == 0) {
+            location.replace('#'); // redirect a disconnected user to the home page
+        } else {
+            location.replace('profile.html'); // redirect a connected user to the home page
+        }
+    });
+
 });
 
-// bed icon function
-nav_bed_btn.addEventListener("click", () => {
-    if (isManager == -1) {
-        location.replace('welcome-page.html'); // redirect a disconnected user to the home page
-    } else {
-        location.replace('home-page.html'); // redirect a connected user to the home page
-    }
-});
 
-// function to send user to right profile page
-nav_profile_btn.addEventListener("click", () => {
-    if (isManager == 0) {
-        location.replace('#'); // redirect a disconnected user to the home page
-    } else {
-        location.replace('profile.html'); // redirect a connected user to the home page
-    }
-});
+// gets current user and fix the navbar accordenly
+function handleNavBar(isManager) {
+    fbAuth.onAuthStateChanged((user) => {
+        if (user) {
+            // a user is connected
+            console.log('user connected');
+
+            // check if manager or customer
+            isManager = checkUserConnected(user);
+            if (isManager == 1) {
+                document.getElementById('nav_home_btn').style.visibility = 'hidden';
+                document.getElementById('nav_catalog_btn').style.visibility = 'hidden';
+                document.getElementById('nav_cart_and_badge').style.visibility = 'hidden';
+                document.getElementById('nav_wishlist_btn').style.visibility = 'hidden';
+                document.getElementById('nav_profile_btn').style.visibility = 'hidden';
+                document.getElementById('nav_login_link').style.visibility = 'hidden';
+                document.getElementById('nav_register_link').style.visibility = 'hidden';
+            }
+
+            // check if customer
+            if (isManager != -1) {
+                document.getElementById('nav_login_link').style.visibility = 'hidden';
+                document.getElementById('nav_register_link').style.visibility = 'hidden';
+            }
+
+            // no user connected or failed to read user from db
+            else {
+                navbarForNoUserConnected();
+                signOutUser();
+            }
+
+        } else {
+            // User is signed out
+            isManager = -1;
+            navbarForNoUserConnected();
+        }
+    });
+    return isManager;
+}
+
+
 
 // log out function
+function signOutUser() {
+    fbAuth.signOut().then(() => {
+        location.replace('welcome-page.html');
+    }).catch((error) => {
+        alert('failed to logout');
+        console.log('Logout err: ', error);
+    });
+}
+
+function checkUserConnected(user) {
+    // return 0 if a customer is connected and 1 if a manager is connected
+    // return -1 if reading the user from collection failed.
+    dbManager.doc(user.email)
+        .get().then((doc) => {
+            if (doc.exists) {
+                console.log('The manager is connnected');
+                return 1;
+            }
+        }).catch((error) => {
+            console.log('failed to read from manager collection:', error);
+        });
+
+    dbCustomers.doc(user.email)
+        .get().then((doc) => {
+            if (doc.exists) {
+                console.log('The user', user.email, 'is connnected');
+                return 0;
+            }
+            else {
+                return -1;  // failed to identify user
+            }
+        }).catch((error) => {
+            console.log('failed to read from customers collection:', error);
+            return -1;  // failed to identify user
+        });
+}
+
+function navbarForNoUserConnected() {
+    console.log('no user connected');
+    document.getElementById('nav_home_btn').style.visibility = 'hidden';
+    document.getElementById('nav_catalog_btn').style.visibility = 'hidden';
+    document.getElementById('nav_cart_and_badge').style.visibility = 'hidden';
+    document.getElementById('nav_wishlist_btn').style.visibility = 'hidden';
+    document.getElementById('nav_profile_btn').style.visibility = 'hidden';
+    document.getElementById('nav_logout_btn').style.visibility = 'hidden';
+}
--- src/index.js
+++ src/index.js
@@ -1,60 +1,77 @@
-const bodyParser = require('body-parser');
+import bodyParser from 'body-parser';
 
-const express = require('express');
+import express from 'express';
+import { json, urlencoded } from 'express';
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 // ** start guide
-const morgan = require('morgan');
-const { Router } = require('express');
+import morgan from 'morgan';
+import { Router } from 'express';
 // ** end guide
 
 const port = process.env.PORT || 5000;
 const app = express();
 // app.use(Router); // line doesn't work
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(__dirname + '/'));
+// app.use('/public', express.static('/'));
 
 // ** start guide
 app.use(morgan('dev'));
 
-app.use(express.json());
+app.use(json());
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  urlencoded({
+    extended: true,
+  })
 );
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
 });
 // ** end guide
 
+// original
 app.get('/', (req, res) => {
-    // res.send('<h1>The web is on the air<h1>');
-    res.sendFile('/components/profile.html', { root: __dirname });
+  // res.send('<h1>The web is on the air<h1>');
+  res.sendFile('/components/welcome-page.html', { root: __dirname });
 });
 
+// test
+// app.get('/', (req, res) => {
+//   // res.send('<h1>The web is on the air<h1>');
+//   res.sendFile(__dirname + '/src/components/welcome-page.html');
+// });
+
 app.listen(port, () => {
-    console.log('server is up and running - checking');
+  console.log('server is up and runing- checking');
 });
 
 // ** start guide
 app.use((req, res, next) => {
-    const error = new Error('Not Found');
-    error.status = 404;
-    next(error);
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
 });
 
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message,
-        },
-    });
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 // ** end guide
