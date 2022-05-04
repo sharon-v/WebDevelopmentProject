219ad78a47608a0664e42235d39eccf8714ec1d7
sharon <sharonvazana@gmail.com>
2022-05-04 20:21:35 +0300

    edit product picture fixed and text centered

--- src/components/manager-edit-product.js
+++ src/components/manager-edit-product.js
@@ -1,6 +1,17 @@
-import {
-    dbProducts
-} from '../firebase/data.js';
+import { dbProducts } from '../firebase/data.js';
+
+/* code to displaying picture in add/edit new product page */
+const image_input = document.querySelector('#image-input');
+
+image_input.addEventListener('change', function() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+        const uploaded_image = reader.result;
+        document.querySelector('#display-image').style.backgroundImage = `url(${uploaded_image})`;
+    });
+    reader.readAsDataURL(this.files[0]);
+});
+/* end - code to displaying picture in add/edit new product page */
 
 console.log('enter');
 
@@ -36,9 +47,5 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(isJustLandedCbChecked);
         console.log(isOnSaleCbChecked);
         console.log(fabric);
-
-        
-
     });
 });
-
