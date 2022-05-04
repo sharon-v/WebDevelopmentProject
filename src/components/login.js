127aaf2faf2d9434a0f22885c531456310a96145
chenBenTolila <chenbt266@gmail.com>
2022-05-04 11:11:53 +0300

    finished implementing login with firebase

--- src/components/login.js
+++ src/components/login.js
@@ -1,59 +1,34 @@
 import { fbAuth } from '../firebase/data.js';
 
-// Set database variable
-//var database = firebase.firestore();
-// document.addEventListener('DOMContentLoaded', () => {
-//     console.log('in');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('in');
 
-// const registerForm = document.querySelector('#regestraion');
-var btn = document.getElementById('loginBtn');
-// when press on sign up button
-btn.addEventListener('click', () => {
-    //e.preventDefault();
-    console.log('clicked on register');
-    const name = document.getElementById('name').value;
-    const password = document.getElementById('password').value;
-    // if (checkEmail(email) == false) {
-    //     // write password confirmation message
-    // }
+    var btn = document.getElementById('loginBtn');
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('clicked on login');
+        const email = document.getElementById('loginEmail').value;
+        const password = document.getElementById('loginPassword').value;
 
-    // if (checkPasswordConfiramtion(password, passwordConfirmation) == false) {
-    //     // write password confirmation message
-    // }
+        console.log(email);
+        console.log(password);
+        login(email, password);
+    });
 
-    console.log(name);
-    console.log(password);
+});
+
+function login(email, password) {
     fbAuth
-        .signInWithEmailAndPassword(name, password)
+        .signInWithEmailAndPassword(email, password)
         .then((userCredential) => {
             // Signed in
             var user = userCredential.user;
-            console.log('success, ', user.uid);
-            location.replace("home-page.html");
+            console.log('success, ', user.email);
+            location.replace('home-page.html');
         })
         .catch((error) => {
             console.log('fail');
             var errorMessage = error.message;
             alert(errorMessage);
-        });
-
-});
-
-
-function login() {
-    var email = document.getElementById("login_email").value;
-    var password = document.getElementById("login_pass").value;
-    firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Signed in
-            var user = userCredential.user;
-            location.replace("registered_home.html");
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            alert(errorCode);
         });
 }
