f3b121c5a9fab7866cd56d3b2ac37eb0c585b8d7
hadar.amsalem <73754433+hadaramsi@users.noreply.github.com>
2022-05-05 21:34:53 +0300

    add product to DB

--- src/components/manager-edit-product.js
+++ src/components/manager-edit-product.js
@@ -49,3 +49,60 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(fabric);
     });
 });
+
+
+function changeValues(Pname, description, price, size90x200, size120x200, size160x200, size180x200, isJustLandedCbChecked,isOnSaleCbChecked, fabric, ImagesRef, sale){
+    console.log(Pname);
+    console.log(description);
+    console.log(price);
+    console.log(sale);
+    console.log(sku);
+    console.log(size90x200);
+    console.log(size120x200);
+    console.log(size160x200);
+    console.log(size180x200);
+    console.log(isJustLandedCbChecked);
+    console.log(isOnSaleCbChecked);
+    console.log(fabric);
+
+    element.removeAttribute('hidden')
+    let Number = element.querySelector('#orderNumber');
+    Number.innerHTML = orderNumber;
+    let orderDate = element.querySelector('#orderDate');
+    orderDate.innerHTML = new Intl.DateTimeFormat('en-GB').format(date.toDate());
+    let buyerName = element.querySelector('#buyerName');
+    
+    //get product by sku
+    dbProducts.where("sku", "==", sku).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            sku.innerHTML = String(doc.data().Pname);
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+    let amount = element.querySelector('#orderAmount');
+    amount.innerHTML = totalAmount.toFixed(2);
+    let selectOp = element.querySelector('#selectOp');
+    let currentTime = Date.now();
+    let differ = Date.now() - date.toDate() ;
+    let Difference_In_hours =Math.ceil(differ / (1000 * 3600 * 24));
+    if (Difference_In_hours > 1){
+        selectOp.disabled = false;
+    }
+    else{
+        selectOp.disabled = true;
+    }
+    selectOp.addEventListener('change', () => {
+        if(selectOp.options[ selectOp.selectedIndex ].value == 2){
+            //needs to update the order status
+        }
+    })
+    var orderPage = element.querySelector('#orderPage');
+    orderPage.addEventListener('click', () => {
+        sessionStorage.setItem('orderNumber', orderNumber); //moving parameters to order summery page
+        location.replace('../components/order-summary.html');
+    })
+    return element;
+}
