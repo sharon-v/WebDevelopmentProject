616fbf3d98bc2930d00bd0686f4e82f8d941015e
chenBenTolila <chenbt266@gmail.com>
2022-05-10 15:27:38 +0300

    shopping cart changes

--- src/components/shopping-cart.js
+++ src/components/shopping-cart.js
@@ -1,129 +1,111 @@
-import { fbAuth, dbshoppingCart, dbProducts } from '../firebase/data.js'
-initialization();
+import { fbAuth, dbShoppingCart, dbProducts } from '../firebase/data.js'
 
+var userShoppingCart = [];
+initialization();
+var firstProductAdded = false;
+let totalItems = 0;
+let totalAmount = 0;
+const subTotal = document.getElementById('subTotal');
+const items = document.getElementById('totalItems');
 
 
 function initialization() {
-    let totalItems = 0;
-    let totalAmount = 0;
-    const subTotal = document.getElementById('subTotal');
-
-    const items = document.getElementById('totalItems');
-
     fbAuth.onAuthStateChanged((user) => {
-        dbshoppingCart.doc(user.email).get().then((querySnapshot) => {
-            var userShoppingCart = querySnapshot.data().productList;
-            for (var i = 0; i < userShoppingCart.length; ++i) {
-                totalItems = totalItems + userShoppingCart[i].quantity;
-                totalAmount = totalItems + userShoppingCart[i].price
+        dbShoppingCart.doc(user.email).get().then((querySnapshot) => {
+            if (querySnapshot.exists) {
+                userShoppingCart = querySnapshot.data().productList;
+                for (let i = 0; i < userShoppingCart.length; ++i) {
+                    console.log(userShoppingCart[i].productId);
+                    dbProducts.doc(userShoppingCart[i].name).get().then((product) => {
+                        if (product.exists) {
+
+                            console.log('product name', product.id);
+                            console.log('product sku', product.data().sku);
+                            console.log('product price', product.data().price);
+                            console.log('product quantity', userShoppingCart[i].quantity);
+                            console.log('product size', userShoppingCart[i].size);
+                            console.log('product url', product.data().imageUrl);
+
+                            if (i == 0) {
+                                editElement(product.id, product.data().sku, product.data().price, userShoppingCart[i].quantity, product.data().imageUrl, userShoppingCart[i].size);
+                            }
+                            else {
+                                addElement(product.id, product.data().sku, product.price, userShoppingCart[i].quantity, product.data().imageUrl, userShoppingCart[i].size);
+                            }
+                            editOrderSummary(product.data().totalItems, doc.data().totalAmount);
+                        }
+                        else {
+                            console.log
+                            editOrderSummary(doc.data().totalItems, doc.data().totalAmount);
+                            spinner.style.display = 'none';
+                        }
+                    })
+                }
             }
-            items.innerHTML = totalItems;
-            subTotal.textContent = totalAmount + '₪';
-            document.getElementById('mainElement').style.display = 'inline';
-            // document.getElementById('spinner').style.display = 'none';
         })
     })
+}
 
-    //     dbOrdersTimes.get().then((querySnapshot) => {
-    //         var counter = 0;
-    //         querySnapshot.forEach((doc) => {
-    //             var date = document.getElementById('dateSelect');
-    //             if (Date.now() < new Date(doc.id).getTime()) { //
-    //                 counter = counter + 1;
-    //                 var opt = document.createElement('option');
-    //                 opt.value = counter;
-    //                 opt.innerHTML = doc.id;
-    //                 date.appendChild(opt);
-    //             }
-    //         });
-    //     });
-    // }
+function editElement(name, SKU, price, quantity, url, size) {
+    let ele = document.querySelector('#product');
+    ele = changeValues(ele, name, SKU, price, quantity, url, size);
+}
 
+function changeValues(element, Name, SKU, price, quantity, url, size) {
+    element.removeAttribute('hidden');
+    let productName = element.querySelector('#productName');
+    productName.innerHTML = Name;
+    let sku = element.querySelector('#productSKU');
+    sku.innerHTML = SKU;
+    let productPrice = element.querySelector('#productPrice');
+    productPrice.innerHTML = price + '₪';
+    let productQuantity = element.querySelector('#productQuantity');
+    productQuantity.innerHTML = quantity;
+    let productSize = element.querySelector('#productSize');
+    productSize.innerHTML = size;
+    // let totalproductPrice = element.querySelector('#totalproductPrice');
+    // totalproductPrice.innerHTML = (price * quantity) + '₪';
+    //TODO: add image ref
+    return element;
+}
 
-    // var date = document.getElementById('dateSelect');
-    // var hours = document.getElementById('hourSelect');
-    // date.addEventListener('change', (e) => {
-    //     //deleting the old hours
-    //     while (hours.children.length > 1) {
-    //         hours.removeChild(hours.lastChild);
-    //     }
-    //     dbOrdersTimes.doc(date.options[date.selectedIndex].text).get().then((querySnapshot) => {
-    //         for (let index = 0; index < querySnapshot.data().hours.length; ++index) {
-    //             var opt = document.createElement('option');
-    //             opt.innerHTML = querySnapshot.data().hours[index];
-    //             hours.appendChild(opt);
-    //         }
-    //     });
-    // })
+function addElement(Name, SKU, price, quantity, url, size) {
+    let ele = document.querySelector('#product');
+    let newElement = ele.cloneNode(true);
+    newElement = changeValues(newElement, Name, SKU, price, quantity, url);
+    let currentDiv = document.getElementById("productsList");
+    currentDiv.appendChild(newElement);
+}
 
+function editOrderDetails(firstName, lastName, street, streetNumber, apartmentNumber, city, postalCode, phoneNumber, shippingDate, shippingHours, notes) {
+    var element = document.querySelector('#orderDetails');
+    element.removeAttribute('hidden');
+    element.querySelector('#fullName').innerHTML = firstName + " " + lastName;
+    if (apartmentNumber == "") {
+        element.querySelector('#address').innerHTML = street + " " + streetNumber + ", " + city;
+    }
+    else {
+        element.querySelector('#address').innerHTML = street + " " + streetNumber + "/" + apartmentNumber + ", " + city;
+    }
+    element.querySelector('#postalCode').innerHTML = postalCode;
+    element.querySelector('#phoneNumber').innerHTML = phoneNumber;
+    element.querySelector('#shippingDate').innerHTML = shippingDate;
+    element.querySelector('#shippingHours').innerHTML = shippingHours;
+    if (notes == "")
+        element.querySelector('#notes').innerHTML = 'The costumer didn`t enter any notes';
+    else
+        element.querySelector('#notes').innerHTML = notes;
+}
 
-    // var btn = document.getElementById('payment_pay_button');
-    // // when press on sign up button
-    // btn.addEventListener('click', (e) => {
-    //     e.preventDefault();  // IMPORTANT! so the db functions could work, DO NOT REMOVE
-    //     console.log('clicked on pay');
-
-    //     // getting the order personal details from the html
-    //     const fname = document.getElementById('orderFname').value;
-    //     const lname = document.getElementById('orderLname').value;
-    //     const street = document.getElementById('orderStreet').value;
-    //     const streetNumber = document.getElementById('orderStreetNumber').value;
-    //     const apartment = document.getElementById('orderApartment');
-    //     if (apartment.length == 0)
-    //         apartment.value = 0;
-    //     let temp = document.getElementById('city');
-    //     const city = temp.options[temp.selectedIndex];
-    //     const postalCode = document.getElementById('postalCode').value;
-    //     const PhoneNumber = document.getElementById('PhoneNumber').value;
-    //     const notes = document.getElementById('notes');
-    //     if (notes.length == 0)
-    //         notes.value = '';
-    //     const orderDate = date.options[date.selectedIndex];
-    //     const orderHours = hours.options[hours.selectedIndex];
-    //     temp = document.getElementById('numberOfPayments');
-    //     const numOfPayments = temp.options[temp.selectedIndex];
-    //     const ID = document.getElementById('Card_owner`s_ID').value;
-    //     temp = document.getElementById('expirationMonth');
-    //     const expirationMonth = temp.options[temp.selectedIndex];
-    //     temp = document.getElementById('expirationYear');
-    //     const expirationYear = temp.options[temp.selectedIndex];
-    //     const cardNumber = document.getElementById('card_number').value;
-    //     const cvc = document.getElementById('cvc').value;
-    //     let res = checkData(fname, lname, street, streetNumber, postalCode, city, orderDate, orderHours, expirationMonth, expirationYear, ID, cardNumber, cvc);
-    //     if (res == true) {
-    //         console.log("true");
-    //         fbAuth.onAuthStateChanged((user) => {
-    //             // Add a new document in collection "orders"
-    //             let productsList;
-    //             dbshoppingCart.doc(user.email).get().then((doc) => {
-    //                 productsList = doc.data().productList;
-    //                 console.log(productsList);
-    //                 const now = new Date(Date.now()).getTime();
-    //                 dbOrders.doc(fname + now).set({
-    //                     buyerEmail: user.email,
-    //                     orderStatus: "Aprroved",
-    //                     purchaseDate: now,
-    //                     totalAmount: document.getElementById('totalPrice').textContent,
-    //                     totalItems: document.getElementById('totalItems').textContent,
-    //                     productsList: productsList,
-    //                 })
-    //                     .then(() => {
-    //                         console.log("Document successfully written!");
-    //                         sessionStorage.setItem('orderNumber', fname + now); //moving parameters to order summery page
-    //                         location.replace('../components/order-summary.html');
-    //                     })
-    //                     .catch((error) => {
-    //                         console.error("Error writing document: ", error);
-    //                     });
-    //             })
-    //         })
-
-    //     }
-    // });
-
-
+function editOrderSummary(quantity, price) {
+    var element = document.querySelector('#orderSummary');
+    element.removeAttribute('hidden');
+    element.querySelector('#totalOrder').innerHTML = price;
+    element.querySelector('#totalproducts').innerHTML = quantity;
+    element.querySelector('#totalPrice').innerHTML = price;
 
 }
+
 
 // create that the amount of product the user want to add is available in the stock
 function checkStock() {
